762e910d2afa46e39d8cf4031fab9038d26dc9be
nayla <naylahsn24@gmail.com>
2026-02-23 13:24:50 +0700

    MTitik

---
+++ Pertemuan1/src/MTitik.java
@@ -0,0 +1,16 @@
+/* Nama File : MTitik.java
+ * Deskripsi : berisi atribut dan method dalam class Titik
+ * Pembuat     : Nayla Husna/24060124140158 
+ * Tanggal     : 23 Februari 2026
+ */
+
+public class MTitik {
+    public static void main(String[] args) {
+        Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
+        T1.setAbsis(3); //mengubah absis T1 dengan nilai 3
+        T1.setOrdinat(4); //mengubah ordinat T1 dengan nilai 4
+        T1.printTitik(); //mencetak koordinat T1 ke layar
+        T1.geser(3,4); //menggeser T1 sejauh (3,4)
+        T1.printTitik(); //menampilkan koordinat T1 setelah digeser
+    }
+}
