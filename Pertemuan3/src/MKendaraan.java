86c917ad78724f23106ecd4c0a6ab471b09fa7a6
nayla <naylahsn24@gmail.com>
2026-03-08 00:39:28 +0700

    Pertemuan-3

---
+++ Pertemuan3/src/MKendaraan.java
@@ -0,0 +1,16 @@
+/* Nama File : MDosen.java
+ * Deskripsi : berisi atribut dan method dalam class Dosen
+ * Pembuat   : Nayla Husna/24060124140158 
+ * Tanggal   : 2 Maret 2026
+ */
+
+public class MKendaraan {
+    public static void main(String[] args){
+        Kendaraan K1 = new Kendaraan();
+        K1.setNoPlat("BA993PN");
+        K1.setJenis("Mobil");
+
+        System.out.println("No Plat kendaraan: " + K1.getNoPlat());
+        System.out.println("Jenis Kendaraan: " + K1.getJenis());
+    }
+}
